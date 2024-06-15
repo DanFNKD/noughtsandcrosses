2fbb07ee15566ce786a00c3e57c95ea4c8279975
Daniel Harris <dwh261@gmail.com>
2024-06-15 11:28:30 +0000

    Created game board

--- run.py
+++ run.py
@@ -3,3 +3,9 @@
 print("Welcome to Noughts & Crosses!")
 print("Let's play:")
 
+# Create the board
+board = {
+    '1': ' ', '2': ' ', '3': ' ',
+    '4': ' ', '5': ' ', '6': ' ',
+    '7': ' ', '8': ' ', '9': ' ',
+}
