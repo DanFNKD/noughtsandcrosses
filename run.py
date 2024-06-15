7983c7af84ec121ec5e68704b10a7c49b7a1e534
Daniel Harris <dwh261@gmail.com>
2024-06-15 13:04:47 +0000

    Added code to check for a winner including the winning combinations

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ def create_board():
     """
     Created the board
     Function returns a dictionary that represents
-    the board
+    the board.
     """
     board = {
     '1': ' ', '2': ' ', '3': ' ',
@@ -18,7 +18,7 @@ def create_board():
 
 def print_board(board):
     """
-    Prints the Noughts & Crosses board
+    Prints the Noughts & Crosses board.
     """
     print(' ' + board['1'] + ' | ' + board['2'] + ' | ' + board['3'] + ' ')
     print('---+---+---')
@@ -29,4 +29,18 @@ def print_board(board):
 board = create_board()
 
 print_board(board)
-    
+
+def check_winner(board, player):
+    """
+    Checks if the player has won.
+    Returns True if the player wins and false if not.
+    """
+    winning_combos = [
+        ['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9'] # Rows
+        ['1', '4', '7'], ['2', '5', '8'], ['3', '6', '9'] # Columns
+        ['1', '5', '9'], ['3', '5', '7'] # Diagonals
+    ]
+    for combo in winning_combos:
+        if all(board[pos] == player for pos in combo):
+            return True
+    return False
