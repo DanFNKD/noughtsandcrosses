48d63f8aa3e18ffedcd4231530fd143e02886c64
Daniel Harris <dwh261@gmail.com>
2024-06-15 11:40:59 +0000

    Added code to print the game board

--- run.py
+++ run.py
@@ -15,3 +15,16 @@ def create_board():
     '7': ' ', '8': ' ', '9': ' ',
 }
 return board
+
+def print_board(board):
+    """
+    Prints the Noughts & Crosses board
+    """
+    print(' ' + board['1'] + ' | ' + board['2'] + ' | ' + board['3'] + ' ')
+    print('---+---+---')
+    print(' ' + board['4'] + ' | ' + board['5'] + ' | ' + board['6'] + ' ')
+    print('---+---+---')
+    print(' ' + board['7'] + ' | ' + board['8'] + ' | ' + board['9'] + ' ')
+
+print_board(board)
+    
